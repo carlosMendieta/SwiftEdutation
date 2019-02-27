d1174c9a8bcf0d71efc59744f68e229c0b589a94
Carlos Mendieta <carlos.mendieta@endava.com>
2019-02-27 10:26:16 -0500

    adding the clas s SpaceAdventure

---
+++ SpaceAdventure-lesson01/SpaceAdventure/SpaceAdventure.swift
@@ -0,0 +1,12 @@
+//
+//  SpaceAdventure.swift
+//  SpaceAdventure
+//
+//  Created by Carlos Mendieta on 2/27/19.
+//  Copyright © 2019 Your School. All rights reserved.
+//
+
+import Foundation
+struct SpaceAdventure {
+    
+}
--- SpaceAdventure-lesson01/SpaceAdventure/main.swift
+++ SpaceAdventure-lesson01/SpaceAdventure/main.swift
@@ -6,6 +6,9 @@
  */
 
 import Foundation
+
+let adventure = SpaceAdventure()
+
 //===============  Lesson 1 ===============
 let numberOfPlanets = 8
 let diameterOfEarth = 24859.82
