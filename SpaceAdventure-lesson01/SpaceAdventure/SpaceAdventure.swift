38bb20f9defd86d134a72d64529ce67d690ef90f
Carlos Mendieta <carlos.mendieta@endava.com>
2019-02-27 11:35:11 -0500

    adding methods to SpaceAdventure class

--- SpaceAdventure-lesson01/SpaceAdventure/SpaceAdventure.swift
+++ SpaceAdventure-lesson01/SpaceAdventure/SpaceAdventure.swift
@@ -7,31 +7,17 @@
 //
 
 import Foundation
-struct SpaceAdventure {
-    let numberOfPlanets = 8
-    let diameterOfEarth = 24859.82
+class SpaceAdventure {
+    
     func start(){
-        welcomeMessage(intNumber: numberOfPlanets, doubleNumber: diameterOfEarth)
-        //===============  Lesson 2 ===============
-        let name = message(message: "What is your name?")
-        print("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
-        var decision = message(message: "Let's go on an adventure! \nShall I randomly choose a planet for you to visit? (Y or N)")
-        let planets = ["mercurio","venus","tierra","marte","jupiter","saturno","urano","neptuno"]
-        while !(decision == "Y" || decision == "N") {
-            decision = getln()
-            if decision == "Y" {
-                print("Ok! Traveling to \(planets.randomElement()!)")
-            } else if decision == "N" {
-                let planet = message(message: "Ok, name the planet you would like to visit...")
-                print("ok, so we are going to \(planet)")
-            } else if decision == "Cookies!"{
-                print("Oh great! I really like cookies!")
-            } else {
-                print("Sorry, I didn't get that.")
-            }
-        }
+        displayIntroduction()
+        greetAdventurer()
+        print("Let's go on an adventure")
+        determineDestination()
     }
-    private func welcomeMessage(intNumber: Int, doubleNumber: Double){
+    private func displayIntroduction(){
+        let numberOfPlanets = 8
+        let diameterOfEarth = 24859.82
         print("Welcome to our solar system!")
         print("There are \(numberOfPlanets) planets to explore.")
         print("You are currently on Earth, which has a circumference of \(diameterOfEarth) miles.")
@@ -39,5 +25,26 @@ struct SpaceAdventure {
     private func message(message: String) -> String {
         print(message)
         return getln()
+    }
+    private func greetAdventurer() {
+        let name = message(message: "What is your name?")
+        print("Nice to meet you, \(name). My name is Eliza, I'm an old friend of Siri.")
+    }
+    private func determineDestination() {
+        let planets = ["mercurio","venus","tierra","marte","jupiter","saturno","urano","neptuno"]
+        var decision = ""
+        while !(decision == "Y" || decision == "N") {
+            decision = message(message: "Shall I randomly choose a planet for you to visit? (Y or N)")
+            if decision == "Y" {
+                print("Ok! Traveling to... \(planets.randomElement()!)")
+            } else if decision == "N" {
+                let planet = message(message: "Ok, name the planet you would like to visit...")
+                print("ok, so we are going to \(planet)")
+            } else if decision == "Cookies!"{
+                print("oh great! I really like cookies!")
+            } else {
+                print("Sorry, I didn't get that.")
+            }
+        }
     }
 }
