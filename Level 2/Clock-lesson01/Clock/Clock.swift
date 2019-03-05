c99c6db145d61ce1f8c1bbfcc3b152ea531d6058
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-05 11:48:23 -0500

    correct display time on screen

---
+++ Level 2/Clock-lesson01/Clock/Clock.swift
@@ -0,0 +1,15 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by Carlos Mendieta on 3/5/19.
+//  Copyright © 2019 Your School. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    var currentTime: NSDate {
+        return NSDate()
+    }
+}
--- Level 2/Clock-lesson01/Clock/ViewController.swift
+++ Level 2/Clock-lesson01/Clock/ViewController.swift
@@ -1,25 +1,27 @@
 /*
-
-This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
-4.0 International License, by Yong Bakos.
-
-*/
+ 
+ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+ 4.0 International License, by Yong Bakos.
+ 
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
+    let clock = Clock()
     
     @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-         print("The view did load.")
-        timeLabel.text = "Current Time"
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        timeLabel.text = formatter.string(from: clock.currentTime as Date)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
