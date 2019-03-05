b6c6e801eb56b8344e34dd0b77f467ec56502d36
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-05 11:13:51 -0500

    programmatically creating a UILabel and set its position and text property

--- Level 2/Clock-lesson01/Clock/ViewController.swift
+++ Level 2/Clock-lesson01/Clock/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
          print("The view did load.")
         timeLabel.text = "Current Time"
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.text = "New label"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
