4ffc2e3551a7cc67e86e74f5bb79c3fe9e0c545f
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-05 17:20:57 -0500

    Using notification center to updte the time at the UIlabel

--- Level 2/Clock-lesson01/Clock/AppDelegate.swift
+++ Level 2/Clock-lesson01/Clock/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
+        print("enter foreground")
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
--- Level 2/Clock-lesson01/Clock/ViewController.swift
+++ Level 2/Clock-lesson01/Clock/ViewController.swift
@@ -11,19 +11,34 @@ class ViewController: UIViewController {
     let clock = Clock()
     
     @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerForNotifications()
     }
+    
+    private func registerForNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateClock),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        timeLabel.text = formatter.string(from: clock.currentTime as Date)
+        updateClock()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func updateClock() {
+        print("La app está activa")
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        timeLabel.text = formatter.string(from: clock.currentTime as Date)
     }
     
 }
