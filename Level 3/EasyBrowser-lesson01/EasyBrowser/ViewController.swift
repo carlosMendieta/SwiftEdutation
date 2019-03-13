694af9372b8ec123cb9d935a5000952ca2bea704
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-13 10:35:08 -0500

    changin openPage method to open the url on the webView

--- Level 3/EasyBrowser-lesson01/EasyBrowser/ViewController.swift
+++ Level 3/EasyBrowser-lesson01/EasyBrowser/ViewController.swift
@@ -6,9 +6,12 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 */
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,9 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func openPageInSafari(_ sender: UIButton) {
+    @IBAction func openPage(_ sender: UIButton) {
         if let url = URL(string: "http://developer.apple.com"){
-            UIApplication.shared.open(url)
+//            UIApplication.shared.open(url)
+            let request = URLRequest(url: url)
+            webView.load(request)
         }
     }
     
