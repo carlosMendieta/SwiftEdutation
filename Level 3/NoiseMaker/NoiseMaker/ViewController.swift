c4b2dd2ad9fbf2e32b577da5811d13de810976f9
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-14 16:37:15 -0500

    implementing the play methods

--- Level 3/NoiseMaker/NoiseMaker/ViewController.swift
+++ Level 3/NoiseMaker/NoiseMaker/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func playGuitar(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "guitar", withExtension: "wav"){
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+    }
+    
+    @IBAction func playApplause(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "applause", withExtension: "wav"){
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+    }
+    
+    @IBAction func playMonster(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "monster", withExtension: "wav"){
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+    }
+    
+    @IBAction func playBubbles(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "bubbles", withExtension: "wav"){
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+    }
+    
 }
 
