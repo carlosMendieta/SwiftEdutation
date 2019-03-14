ebfcc03a07cead893e84896ce0b3d5990de21506
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-14 14:27:35 -0500

    adding a userTrakingMode in the viewDidLoad, adding a delegate whehn the user location is updated

--- Level 3/Found-lesson01/Found/ViewController.swift
+++ Level 3/Found-lesson01/Found/ViewController.swift
@@ -6,17 +6,29 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 */
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.setUserTrackingMode(.follow, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let width = 1000.0 //meters
+        let height = 1000.0
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: width, longitudinalMeters: height)
+        mapView.setRegion(region, animated: true)
     }
 
     
