ceceae7966026ee4efc36dbdc018828bf19593b4
Carlos Mendieta <carlos.mendieta@endava.com>
2019-03-15 16:17:19 -0500

    replacing the four controller actions with one new playSound action and passing the sender parameter tag property value as the argument

--- Level 3/NoiseMaker/NoiseMaker/ViewController.swift
+++ Level 3/NoiseMaker/NoiseMaker/ViewController.swift
@@ -18,20 +18,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func playGuitar(_ sender: UIButton) {
-        noiseMaker.play(index: 0)
-    }
-    
-    @IBAction func playApplause(_ sender: UIButton) {
-        noiseMaker.play(index: 1)
-    }
-    
-    @IBAction func playMonster(_ sender: UIButton) {
-        noiseMaker.play(index: 2)
-    }
-    
-    @IBAction func playBubbles(_ sender: UIButton) {
-        noiseMaker.play(index: 3)
+    @IBAction func playSound(_ sender: UIButton) {
+        noiseMaker.play(index: sender.tag)
     }
     
 }
